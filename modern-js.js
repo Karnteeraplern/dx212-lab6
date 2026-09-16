bf07b6a71beb548a0f0597dbe503632d4851ad85
Karnteera Itipcharoen) <plern2549@gmail.com)>
2026-09-16 10:57:10 +0700

    first commit

---
+++ modern-js.js
@@ -0,0 +1,6 @@
+function greet(name, faculty) {
+    return "สวัสดี " + name + " จากคณะ " + faculty + "!";
+}
+const greet_modern = (name, faculty) => `สวัสดี ${name} จากคณะ ${faculty}!`;
+console.log(greet("plern", "CITU"));
+console.log(greet_modern("plern", "CITU"));
